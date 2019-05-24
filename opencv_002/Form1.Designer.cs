5cd3e6c06cf418e5efdd250048dc18c51c906a4f
dev-yym <dev-yym@LAPTOP-J4U0QGE6>
2019-05-24 17:51:40 +0900

    1

---
+++ opencv_002/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace opencv_002
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.log_1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.Location = new System.Drawing.Point(1147, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 480);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // log_1
+            // 
+            this.log_1.Location = new System.Drawing.Point(646, 0);
+            this.log_1.Multiline = true;
+            this.log_1.Name = "log_1";
+            this.log_1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.log_1.Size = new System.Drawing.Size(495, 480);
+            this.log_1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button2.Location = new System.Drawing.Point(1147, 52);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(155, 34);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "STOP";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1314, 776);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.log_1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox log_1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ opencv_002/Form1.cs
@@ -0,0 +1,115 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace opencv_002
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        VideoCapture videoCapture;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            for(int deviceNum=1; 99999 > deviceNum; deviceNum++)
+            {
+                videoCapture = new VideoCapture(deviceNum);
+                if (videoCapture.IsOpened() == true)
+                {
+                    log_write("카메라 연결 성공 DeviceID : " + Convert.ToString(deviceNum));
+                    break;
+                }
+            }
+
+            videoCapture.Set(CaptureProperty.FrameWidth, 640);
+            videoCapture.Set(CaptureProperty.FrameHeight, 480);
+
+            log_write("videoCapture.FrameWidth:" + Convert.ToString(videoCapture.FrameWidth));
+            log_write("videoCapture.FrameHeight:" + Convert.ToString(videoCapture.FrameHeight));
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            log_1.Text = null;
+            log_write("START BUTTON CLICK");
+
+            timer1.Stop();
+            timer1.Interval = 50; //스케쥴 간격 설정
+            timer1.Start(); //타이머를 가동시킨다.
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (videoCapture.IsOpened() == false)
+            {
+                log_write("카메라 연결 안됨");
+            }
+
+            Mat frame_img_source_01 = new Mat();
+            videoCapture.Read(frame_img_source_01);
+            
+            pictureBox1.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(frame_img_source_01);
+
+            // 그레이 스케일
+            Mat frame_img_source_02 = new Mat();
+            videoCapture.Read(frame_img_source_02);
+            Mat frame_img_gray = new Mat();
+            byte[] imageBytes = frame_img_source_02.ToBytes(".bmp");
+            //byte[] imageBytes = frame_img_FlipY.ToBytes(".bmp");
+            frame_img_gray = Mat.FromImageData(imageBytes, ImreadModes.Grayscale);
+            pictureBox1.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(frame_img_gray);
+
+            Cv2.EqualizeHist(frame_img_gray, frame_img_gray);
+            var cascade = new CascadeClassifier("C://Users//dev-yym//source//repos//opencv_002//FaceML_Data//haarcascade_frontalface_alt.xml");
+            //var nestedCascade = new CascadeClassifier("C://Users//dev-yym//source//repos//opencv_002//FaceML_Data//haarcascade_eye_tree_eyeglasses.xml");
+
+
+            var faces = cascade.DetectMultiScale(
+                image: frame_img_gray,
+                scaleFactor: 1.1,
+                minNeighbors: 2,
+                flags: HaarDetectionType.DoRoughSearch | HaarDetectionType.ScaleImage,
+                minSize: new OpenCvSharp.Size(30, 30)
+                );
+
+            log_write("Detected faces:" + Convert.ToString(faces.Length));
+
+
+            for(int faceCnt=0; faceCnt < faces.Length; faceCnt++)
+            {
+
+                log_write("faces:" + Convert.ToString(faces[faceCnt]));
+
+                Cv2.Rectangle(frame_img_source_01, faces[faceCnt], Scalar.YellowGreen, 2);
+
+            }
+            pictureBox1.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(frame_img_source_01);
+        }
+
+        private void log_write(String logText)
+        {
+            log_1.Text = Convert.ToString(DateTime.Now) + "-" +logText + "\r\n" + log_1.Text;
+
+        }
+
+
+    }
+}
